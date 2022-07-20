698f457e5e9bd3780917b7b4b990b6525b58b78f
lukavelic <96191586+lukavelic@users.noreply.github.com>
2022-07-20 21:13:55 +0200

    Added method to fetch weather data using location info

--- script.js
+++ script.js
@@ -1,20 +1,39 @@
 class Weather {
     constructor() {
-        this.data = {}
+        this.location = {}
+        this.weatherData = {}
     }
-    geocode = (query) => {
+    geocode = (input) => {
         const apiKey = 'f8f7a86acb216286bdcaa84ea257f83c';
         const resultLimit = 1;
+        const query = input;
 
         const apiCall = async (query) => {
             try {
-                const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=${resultLimit}&appid=${apiKey}`);
+                const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=${resultLimit}&appid=${apiKey}`, {mode: 'cors'});
+                const location = await response.json();
+                const firstResult = location[0];
+                
+                this.location = firstResult;
+
+                this.getWeatherData();
+            } catch (error) {
+                console.log(error);
+            };
+        };
+
+        apiCall(query);
+    };
+    getWeatherData = () => {
+        const apiKey = 'f8f7a86acb216286bdcaa84ea257f83c';
+
+        const apiCall = async () => {
+            try {
+                console.log(this)
+                const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${this.location.lat}&lon=${this.location.lon}&appid=${apiKey}`, {mode: 'cors'});
                 const weatherData = await response.json();
-                const firstResult = weatherData[0];
-                
-                this.data = firstResult;
-                
-                console.log(this.data)
+
+                this.weatherData = weatherData;
             } catch (error) {
                 console.log(error);
             };
