802356ca042fe423e65877d0e26959ec1a0e6804
lukavelic <96191586+lukavelic@users.noreply.github.com>
2022-07-20 22:03:24 +0200

    Added basic search results under search box

--- script.js
+++ script.js
@@ -8,32 +8,40 @@ class Weather {
         const searchButton = document.querySelector('#search-button');
         const searchBox = document.querySelector('#search-box');
         searchButton.addEventListener('click', this.getInput);
+        searchBox.addEventListener('input', function(e) {
+            obj.geocode(e.target.value);
+        } );
         searchBox.addEventListener('keypress', function(e) {
             if(e.key === 'Enter') {
                 obj.getInput();
             };
         });
     };
+    
+    displayResults = () => {
+        const resultsDiv = document.querySelector('#results');
+        console.log(this)
+        resultsDiv.innerText = `${this.location[0].name}, ${this.location[1].name}, ${this.location[2].name}`;
+        
+    };
     getInput = () => {
         const searchBox = document.querySelector('#search-box');
         this.geocode(searchBox.value);
         searchBox.value = '';
-
     };
     geocode = (input) => {
         const apiKey = 'f8f7a86acb216286bdcaa84ea257f83c';
-        const resultLimit = 1;
+        const resultLimit = 3;
         const query = input;
 
         const apiCall = async (query) => {
             try {
                 const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=${resultLimit}&appid=${apiKey}`, {mode: 'cors'});
                 const location = await response.json();
-                const firstResult = location[0];
-                
-                this.location = firstResult;
+                this.location = location;
 
                 this.getWeatherData();
+                this.displayResults();
             } catch (error) {
                 console.log(error);
             };
@@ -47,7 +55,7 @@ class Weather {
         const apiCall = async () => {
             try {
                 console.log(this)
-                const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${this.location.lat}&lon=${this.location.lon}&appid=${apiKey}`, {mode: 'cors'});
+                const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${this.location[0].lat}&lon=${this.location[0].lon}&appid=${apiKey}`, {mode: 'cors'});
                 const weatherData = await response.json();
 
                 this.weatherData = weatherData;
